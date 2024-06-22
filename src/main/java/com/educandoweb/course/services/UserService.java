3470bcff6cc73229091cb5eb4cf73efbe3f73b59
SouzaDiegoCl <souzadiegocl@gmail.com>
2024-06-22 18:46:30 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 //Anotation para registrar classe no spring como service, poderia ser Component ou Repository
 @Service
 public class UserService {
@@ -44,9 +46,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {	
 		User entity = repository.getReferenceById(id);// Prepara objeto monitorado sem operar com banco de dados
 		updateData(entity, obj);
-		return repository.save(entity);
+		return repository.save(entity);	
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
